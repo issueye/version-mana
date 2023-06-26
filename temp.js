1dc2c7185c7c94ccc9b86d89cc131cd39a609474
issueye <issueye@yeah.net>
2023-06-26 22:34:13 +0800

    更新模板文件

--- temp.js
+++ temp.js
@@ -1,101 +1,114 @@
 var exec = require('vmm/exec')
+var filepath = require('std/path/filepath')
 
-class Builder {
-  	// 构造函数
-  	constructor() {
-      	// 命令行执行对象
-    	this.cmd = exec.new()
-      	
-      	this.cmd.setEnv('GOPROXY', 'https://goproxy.cn')
-        this.cmd.setEnv('CGO_ENABLED', '0')
-      	this.cmd.setEnv('GOCACHE', lichee.cacheDir)
-      	
-      	// 获取工作区
-        this.workDir = lichee.workDir
-        // 直接删除文件夹
-        console.log('删除文件夹：', this.workDir)
-        removeExists(this.workDir)
-        // 检查文件夹，没有则创建
-        console.log('创建文件夹：', this.workDir)
-        createNotExists(this.workDir)
+let cmd = exec.new()
+let workDir = lichee.workDir
 
-        // 设置工作区
-        console.log('设置工作区：', this.workDir)
-        this.cmd.setWorkDir(this.workDir)
-    }
-  	
-  	// 拉取代码
-    clone = () => {
-        // 拉取代码
-        console.log('拉取代码')
-      	let data = lichee.vers.Branch.split('/')
-        let branchName = data[data.length - 1]
-        console.log('branchName ', branchName)
-      	
-        this.cmd.run('git', ['clone', '-b', branchName, '-c', 'http.proxy=192.168.227.120:7890', lichee.repo.RepoUrl], function (val) {
-            let data = convToString(val, 0)
-            console.log(data)
-        })
-        // 设置工作区
-        console.log('设置工作区：', this.workDir + '\\' + lichee.repo.ProjectName)
-        this.cmd.setWorkDir(this.workDir + '\\' + lichee.repo.ProjectName)
-    }
-  
-  	// 更新包、拉取到本地
-    pull = () => {
-        console.log('拉取更新引用模块')
-        this.cmd.runWait('go', ['mod', 'tidy'], 10, function (val) {
-            let data = convToString(val, 0)
-            console.log(data)
-        })
-
-        console.log('将引用模块拉取到本项目 vendor 目录')
-        this.cmd.runWait('go', ['mod', 'vendor'], 10, function (val) {
-            let data = convToString(val, 0)
-            console.log(data)
-        })
-    }
-  
-  	// 编译
-    compile = (platform) => {
-      	let goos = ''
-        let appName = `bin/${lichee.vers.AppName}`
-        switch (platform) {
-            case "windows":
-                {
-                    // 编译 windows
-                  	goos = 'windows'
-                  	appName += '.exe'
-                    break;
-                }
-            case "linux":
-                {
-                  	goos = 'linux'
-                    break;
-                }
-        }
-		
-      	let params = [ 'build', '-o', appName, 'main.go' ]
-      	this.cmd.setEnv('GOOS', goos)
-        this.cmd.run('go', params, function (val) {
-            let data = convToString(val, 0)
-            console.log(data)
-        })
-    }
-
-  
-  	// 运行构建任务
-  	run = () => {
-      	// 拉取代码
-      	this.clone()
-      	// 同步依赖
-      	this.pull()
-      	// 编译 windows
-    	this.compile('windows')
-      	// 编译 linux
-      	this.compile('linux')
-    }
+// 设置环境变量
+function setEnv() {
+	cmd.setEnv('GOPROXY', 'https://goproxy.cn')
+    cmd.setEnv('CGO_ENABLED', '0')
+    cmd.setEnv('GOCACHE', lichee.cacheDir)
 }
 
-let build = new Builder()
-build.run()
+// 检查工作区
+function checkPath() {
+    // 直接删除文件夹
+    console.log('删除文件夹：', workDir)
+    removeExists(this.workDir)
+    // 检查文件夹，没有则创建
+    console.log('创建文件夹：', workDir)
+    createNotExists(workDir)
+
+    // 设置工作区
+    console.log('设置工作区：', workDir)
+    cmd.setWorkDir(workDir)
+}
+
+// 拉取代码
+function cloneCode() {
+	let data = lichee.vers.Branch.split('/')
+    let branchName = data[data.length - 1]
+    console.log('branchName ', branchName)
+
+    cmd.run('git', ['clone', '-b', branchName, '-c', 'http.proxy=127.0.0.1:1080', lichee.repo.RepoUrl], function (val) {
+      let data = convToString(val, 0)
+      console.log(data)
+    })
+  
+    // 设置工作区
+  	workDir = workDir + '\\' + lichee.repo.ProjectName
+    console.log('设置工作区：', workDir)
+    cmd.setWorkDir(workDir)
+}
+
+// go mod
+function goMod() {
+	console.log('拉取更新引用模块')
+    cmd.runWait('go', ['mod', 'tidy'], 10, function (val) {
+      let data = convToString(val, 0)
+      console.log(data)
+    })
+
+    console.log('将引用模块拉取到本项目 vendor 目录')
+    cmd.runWait('go', ['mod', 'vendor'], 10, function (val) {
+      let data = convToString(val, 0)
+      console.log(data)
+    })
+}
+
+// 编译
+function compile(platform) {
+	let goos = 'linux'
+    let appName = `bin/${lichee.vers.AppName}`
+    let name = lichee.vers.AppName
+    
+    if (platform == "windows") {
+    	// 编译 windows
+        goos = 'windows'
+        appName += '.exe'
+      	name += '.exe'
+    }
+
+    let params = [ 'build', '-o', appName, 'main.go' ]
+    cmd.setEnv('GOOS', goos)
+    cmd.run('go', params, function (val) {
+      let data = convToString(val, 0)
+      console.log(data)
+    })
+  
+  	console.log(`[${platform}] =>  ${appName} 编译完成`)
+  	
+  	// 将程序路径返回
+  	return name
+}
+
+// 移动程序到下载目录
+function moveApp(appName) {
+  	let tmp = filepath.join(rootPath(), 'runtime', 'static', 'app')
+	let newPath = filepath.join(tmp, lichee.vers.AppName, appName)
+    let oldPath = filepath.join(workDir, 'bin', appName)
+    createNotExists(tmp)
+    moveFile(oldPath, newPath)
+  	console.log(`[${appName}]移动完成`)
+}
+
+// 运行
+function buildApp() {
+  	// 环境变量
+  	setEnv()
+  	// 检查工作区
+  	checkPath()
+	// 拉取代码
+    cloneCode()
+    // 同步依赖
+    goMod()
+    // 编译 windows
+    let windowName = compile('windows')
+  	moveApp(windowName)
+    // 编译 linux
+    let linuxName = compile('linux')
+    moveApp(linuxName)
+}
+
+buildApp()
