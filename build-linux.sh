cf25943e93eb0a727e9ab6451ecc4101ba4aaaaf
issueye <issueye@yeah.net>
2023-08-04 18:04:49 +0800

    ++

--- build-linux.sh
+++ build-linux.sh
@@ -15,9 +15,9 @@ VERSION="v0.1.1-${TAG}"
 APPNAME="VersionMana-${VERSION}"
 DESCRIPTION="版本管理服务"
 
-go build -o bin/${APPNAME} -ldflags "-X demo/build.AppName=Demo \
+go build -o bin/${APPNAME} -tags=ui -ldflags "-X demo/build.AppName=Demo \
 -X github.com/issueye/version-mana/internal/initialize.Branch=${BRANCH} \
 -X github.com/issueye/version-mana/internal/initialize.Commit=${COMMIT} \
 -X github.com/issueye/version-mana/internal/initialize.Date=${NOW} \
 -X github.com/issueye/version-mana/internal/initialize.AppName=${DESCRIPTION} \
--X github.com/issueye/version-mana/internal/initialize.Version=${VERSION}" main.go
+-X github.com/issueye/version-mana/internal/initialize.Version=${VERSION}" .
